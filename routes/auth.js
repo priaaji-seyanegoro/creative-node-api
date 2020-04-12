875b5be09b55fd12a2c95204a9e97828e380da82
priaajisn <priaaji.setyanegoro@gmail.com>
2020-04-13 05:56:34 +0700

    created validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,19 @@
 const route = require("express").Router();
 const User = require("../models/User");
+const { registerValidation } = require("../validation");
 
 route.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
+
+  //VALIDATE BEFORE STORE
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //CHECK USER ALREADY EXIST
+  const emailExits = await User.findOne({ email: email });
+  if (emailExits) return res.status(400).send("email already exist");
+
+  //CREATE NEW USER
   const user = new User({
     name: name,
     email: email,
---
+++ validation.js
@@ -0,0 +1,25 @@
+//VALIDATON
+const Joi = require("@hapi/joi");
+
+//REGISTER
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().required().min(6),
+  });
+
+  return schema.validate(data);
+};
+
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
