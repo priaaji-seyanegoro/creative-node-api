00ce49a4cdbde760c143c93c7cf298a42a73a651
priaajisn <priaaji.setyanegoro@gmail.com>
2020-04-15 12:41:13 +0700

    created route detail,post,update, and detail podcast

--- routes/podcastApi.js
+++ routes/podcastApi.js
@@ -1,4 +1,5 @@
 const route = require("express").Router();
+const User = require("../models/User");
 const Podcast = require("../models/Podcast");
 
 route.get("/", async (req, res) => {
@@ -17,7 +18,7 @@ route.get("/", async (req, res) => {
           request: {
             type: "GET",
             desc: "For get detail podcast",
-            url: `http://localhost:3000/podcast/${podcast._id}`,
+            url: `http://localhost:4000/api/podcast/${podcast._id}`,
           },
         };
       }),
@@ -27,12 +28,38 @@ route.get("/", async (req, res) => {
   }
 });
 
-route.get("/:podcastId", (req, res) => {
-  res.send("detail data podcast ");
+route.get("/:podcastId", async (req, res) => {
+  try {
+    const podcast = await Podcast.findById(req.params.podcastId)
+      .select("_id title description userId date")
+      .populate("userId", "name , email")
+      .exec();
+    if (!podcast)
+      return res.status(404).send({
+        message: "Sorry Podcast not found",
+      });
+
+    res.status(200).send({
+      podcast: podcast,
+      request: {
+        type: "GET",
+        desc: "Get All Data Podcasts",
+        url: "http://localhost:4000/api/podcast",
+      },
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 route.post("/", async (req, res) => {
   const { userId, title, description } = req.body;
+  const user = await User.findById(userId).exec();
+
+  if (!user)
+    return res.status(404).send({
+      message: "Sorry UserId not found",
+    });
   const podcast = new Podcast({
     title: title,
     description: description,
@@ -46,12 +73,57 @@ route.post("/", async (req, res) => {
   }
 });
 
-route.put("/:podcastId", (req, res) => {
-  res.send("update data podcast ");
+route.patch("/:podcastId", async (req, res) => {
+  const id = req.params.podcastId;
+  const { title, description } = req.body;
+  try {
+    const podcast = await Podcast.updateOne(
+      { _id: id },
+      {
+        $set: {
+          title: title,
+          description: description,
+        },
+      }
+    );
+
+    res.send({
+      massage: "Podcast Updated",
+      podcast: podcast,
+      request: {
+        type: "DELETE",
+        desc: "For Delete data",
+        url: `http://localhost:4000/api/podcast/${id}`,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Podcast not found",
+      error: error.message,
+    });
+  }
 });
 
-route.delete("/:podcastId", (req, res) => {
-  res.send("update data podcast ");
+route.delete("/:podcastId", async (req, res) => {
+  try {
+    const removePodcast = await Podcast.deleteOne({
+      _id: req.params.podcastId,
+    });
+    res.send({
+      massage: "Podcast Deleted",
+      podcast: removePodcast,
+      request: {
+        type: "POST",
+        desc: "For Created New Podcast",
+        url: `http://localhost:4000/api/podcast`,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "Podcast not found",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = route;
