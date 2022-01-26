7453d0fc4218fab9a3da4b991359a1d127f8b270
imam.ramadhan <imamramadhan420@gmail.com>
2022-01-26 09:14:24 +0700

    add new route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const app = express();
 
 // Import Routes
+const appRoute = require("./routes/appRoute");
 const authRoute = require("./routes/authRoute");
 const podcastRoute = require("./routes/podcastRoute");
 const postsRoute = require("./routes/posts");
@@ -32,6 +33,7 @@ app.get("/", (req, res) => {
 });
 
 //Route Middleware
+app.use("/api/app", appRoute);
 app.use("/api/user/auth", authRoute);
 app.use("/api/podcast", podcastRoute);
 app.use("/api/posts", postsRoute);
